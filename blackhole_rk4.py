12b95db13573813275e2443d2d3dce6a40b3aa50
Dev Sharma <devsharma@lawn-10-91-73-159.lawn.gatech.edu>
2025-04-12 20:32:10 -0400

    star as particle cloud with gravity black hole

--- blackhole_rk4.py
+++ blackhole_rk4.py
@@ -13,15 +13,11 @@ num_steps = 10000 # number of sim time steps
 # define variables
 m_bh = 10e6 * solar_mass # mass of black hole [kg]
 m_star = 10 * solar_mass # mass of red giant star [kg]
-r_s = 2 * G * m_bh / c**2 # schwarzschild radius of black hole [m]
+r_bh = 2 * G * m_bh / c**2 # schwarzschild radius of black hole [m]
 
 # define initial conditions of star
-x, y = r_s, r_s*5
-v_x, v_y = 7e7, 0
-# r_initial = math.sqrt(x**2 + y**2)
-# v_orbit = math.sqrt(G * m_bh/r_initial)
-# v_x = -v_orbit * (y/r_initial)
-# v_y = v_orbit * (x/r_initial)
+x1, y1 = r_bh*5, r_bh*5
+v_x1, v_y1 = -3e7, 3e7
 
 # define function for force of grav on star in x and y directions
 def f_grav(m1, m2, x, y):
@@ -34,80 +30,91 @@ def f_grav(m1, m2, x, y):
 
 # define plot
 fig, ax = plt.subplots(figsize=(8, 8))
-ax.set_xlim(-r_s*10, r_s*10)
-ax.set_ylim(-r_s*10, r_s*10)
+ax.set_xlim(-r_bh*15, r_bh*15)
+ax.set_ylim(-r_bh*15, r_bh*15)
 
 # plot star, black hole, and event horizon
-star, = ax.plot([], [], 'o', color='black', markersize=3)
+star1, = ax.plot([], [], 'o', markersize=3)
 ax.plot(0, 0, 'o', color='black', markersize=10)
-ax.add_patch(plt.Circle((0, 0), r_s, color='black', fill=False, linestyle='dashed'))
+ax.add_patch(plt.Circle((0, 0), r_bh, color='black', fill=False, linestyle='dashed'))
 
-# add star trace lines
-trail, = ax.plot([], [], '-', color='orange', linewidth=1)
-trail_x, trail_y = [], []
+# add star trail lines
+# trail1, = ax.plot([], [], '-', color='orange', linewidth=1)
+# trail_x1, trail_y1 = [], []
 
 # initialize plot
 def init():
-    star.set_data([], [])
-    return star,
+    star1.set_data([], [])
+    return star1, 
 
 # update plot across time steps
 def update(frame):
-    global x, y, v_x, v_y
+    global x1, y1, v_x1, v_y1
 
-    # calc acceleration from grav in x and y directions
-    def accel(x, y):
-        f_x, f_y = f_grav(m_bh, m_star, x, y)
-        a_x = f_x / m_star
-        a_y = f_y / m_star
-        return a_x, a_y
+    # rk4 method
+    def rk4(x, y, v_x, v_y):
+        # calc acceleration from grav in x and y directions
+        def accel(x, y):
+            f_x, f_y = f_grav(m_bh, m_star, x, y)
+            a_x = f_x / m_star
+            a_y = f_y / m_star
+            return a_x, a_y
 
-    # k1
-    a_x1, a_y1 = accel(x, y)
-    k1_vx = a_x1 * dt
-    k1_vy = a_y1 * dt
-    k1_x = v_x * dt
-    k1_y = v_y * dt
+        # k1
+        a_x1, a_y1 = accel(x, y)
+        k1_vx = a_x1 * dt
+        k1_vy = a_y1 * dt
+        k1_x = v_x * dt
+        k1_y = v_y * dt
 
-    # k2
-    a_x2, a_y2 = accel(x + 0.5 * k1_x, y + 0.5 * k1_y)
-    k2_vx = a_x2 * dt
-    k2_vy = a_y2 * dt
-    k2_x = (v_x + 0.5 * k1_vx) * dt
-    k2_y = (v_y + 0.5 * k1_vy) * dt
+        # k2
+        a_x2, a_y2 = accel(x + 0.5 * k1_x, y + 0.5 * k1_y)
+        k2_vx = a_x2 * dt
+        k2_vy = a_y2 * dt
+        k2_x = (v_x + 0.5 * k1_vx) * dt
+        k2_y = (v_y + 0.5 * k1_vy) * dt
 
-    # k3
-    a_x3, a_y3 = accel(x + 0.5 * k2_x, y + 0.5 * k2_y)
-    k3_vx = a_x3 * dt
-    k3_vy = a_y3 * dt
-    k3_x = (v_x + 0.5 * k2_vx) * dt
-    k3_y = (v_y + 0.5 * k2_vy) * dt
+        # k3
+        a_x3, a_y3 = accel(x + 0.5 * k2_x, y + 0.5 * k2_y)
+        k3_vx = a_x3 * dt
+        k3_vy = a_y3 * dt
+        k3_x = (v_x + 0.5 * k2_vx) * dt
+        k3_y = (v_y + 0.5 * k2_vy) * dt
 
-    # k4
-    a_x4, a_y4 = accel(x + k3_x, y + k3_y)
-    k4_vx = a_x4 * dt
-    k4_vy = a_y4 * dt
-    k4_x = (v_x + k3_vx) * dt
-    k4_y = (v_y + k3_vy) * dt
+        # k4
+        a_x4, a_y4 = accel(x + k3_x, y + k3_y)
+        k4_vx = a_x4 * dt
+        k4_vy = a_y4 * dt
+        k4_x = (v_x + k3_vx) * dt
+        k4_y = (v_y + k3_vy) * dt
 
-    # combine k1, k2, k3, k4
-    x += (k1_x + 2*k2_x + 2*k3_x + k4_x) / 6
-    y += (k1_y + 2*k2_y + 2*k3_y + k4_y) / 6
-    v_x += (k1_vx + 2*k2_vx + 2*k3_vx + k4_vx) / 6
-    v_y += (k1_vy + 2*k2_vy + 2*k3_vy + k4_vy) / 6
+        # combine k1, k2, k3, k4
+        x += (k1_x + 2*k2_x + 2*k3_x + k4_x) / 6
+        y += (k1_y + 2*k2_y + 2*k3_y + k4_y) / 6
+        v_x += (k1_vx + 2*k2_vx + 2*k3_vx + k4_vx) / 6
+        v_y += (k1_vy + 2*k2_vy + 2*k3_vy + k4_vy) / 6
+
+        return x, y, v_x, v_y
     
+    x1, y1, v_x1, v_y1 = rk4(x1, y1, v_x1, v_y1)
+
     # in the case star falls into event horizon
-    if math.sqrt(x**2 + y**2) <= r_s: 
-        x, y = 0, 0
-        v_x, v_y = 0, 0
+    if math.sqrt(x1**2 + y1**2) <= r_bh: 
+        x1, y1 = 0, 0
+        v_x1, v_y1 = 0, 0
 
-    trail_x.append(x)
-    trail_y.append(y)
-    trail.set_data(trail_x, trail_y)
+    # trail_x1.append(x1)
+    # trail_y1.append(y1)
+    # trail1.set_data(trail_x1, trail_y1)
 
-    star.set_data([x], [y])
-    return star, trail
+    speed = math.sqrt(v_x1**2 + v_y1**2)
+    speed_norm = min(speed / 1e8, 1.0) 
+    star_color = (speed_norm, 0.2, 1.0 - speed_norm)
+    star1.set_color(star_color)
+    star1.set_data([x1], [y1])
+
+    return star1, # trail1
 
 # plot animation
-ani = animation.FuncAnimation(fig, update, frames=50, init_func=init, blit=True, interval=10)
+ani = animation.FuncAnimation(fig, update, frames=500, init_func=init, blit=True, interval=10)
 plt.show()
--- starball.py
+++ starball.py
@@ -1,56 +1,137 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# constants
-G = 6.67430e-11  # gravitational constant
-m_star = 2e30     # mass of the star 
-r_star = 7e8      # radius of the star 
-n_par = 50  # number of particles to simulate
+# define constants
+solar_mass = 1.98892e30 # one solar mass [kg]
+solar_radius = 6.957e8 # one solar radius [m]
+c = 2.99792458e8 # speed of light [m/s]
+G = 6.67408e-11 # gravitational constant [m^3 kg^-1 s^-2]
+dt = 10 # time step (s)
+num_steps = 10000 # number of sim time steps
 
-# create particles within a certain radius
-theta = np.linspace(0, 2*np.pi, n_par)
-radii = np.random.uniform(0, r_star, n_par)  # random radii
-x = radii * np.cos(theta)  # x positions
-y = radii * np.sin(theta)  # y positions
+# define variables
+m_bh = 1e6 * solar_mass # mass of black hole [kg]
+m_star = 10 * solar_mass # mass of red giant star [kg]
+r_bh = 2 * G * m_bh / c**2 # schwarzschild radius of black hole [m]
+r_s = solar_radius # radius of star [m]
+n_particles = 1000 # number of particles in star
 
 # mass of particles
-masses = np.ones(n_par) * 1e24
+m_particles = np.ones(n_particles) * m_star / n_particles
 
+# generate particles
+theta = np.linspace(0, 2*np.pi, n_particles)
+radii = np.random.uniform(0, r_s, n_particles)  # random radii
+x1 = radii * np.cos(theta)  # x positions
+x1 += 5*r_bh
+y1 = radii * np.sin(theta)  # y positions
+y1 += 5*r_bh
+v_x1 = np.ones(n_particles) * -3e7
+v_y1 = np.ones(n_particles) * 3e7
+
+# define function for force of grav on star in x and y directions
 def f_grav(m1, m2, x, y):
-    r = math.sqrt(x**2 + y**2)
-    if r == 0: return 0, 0
-    f = -1 * G * m1 * m2 / r**2
-    fx = f * (x / r)
-    fy = f * (y / r)
+    r = np.sqrt(x**2 + y**2)
+    fx = np.zeros(n_particles)
+    fy = np.zeros(n_particles)
+    mask = r != 0
+    f = np.where(mask, -1 * G * m1 * m2 / r**2, 0)
+    fx[mask] = f[mask] * (x[mask] / r[mask]) 
+    fy[mask] = f[mask] * (y[mask] / r[mask]) 
     return fx, fy
 
-def t_force(x,y,r_star, m_star, m_bh):
+# define function for tidal forces across star
+def t_force(x, y, r_s, m_star, m_bh):
+    r_minus = np.sqrt((x-r_s/2) ** 2 + ((y - r_s/2) ** 2))
+    r_plus = np.sqrt((x+r_s/2) ** 2 + ((y + r_s/2) ** 2))
+    ax.plot(-r_bh, 0, 'o', color='black', markersize=10)
 
-    r_minus = np.sqrt((x-r_star/2) ** 2 + ((y - r_star/2) ** 2))
-    r_plus = np.sqrt((x+r_star/2) ** 2 + ((y + r_star/2) ** 2))
-
-    f_x_m, f_y_m = f_grav(m_bh, m_star, x - r_star/2, y)
-    f_x_p, f_y_p = f_grav(m_bh, m_star, x + r_star/2, y)
+    f_x_m, f_y_m = f_grav(m_bh, m_star, x - r_s/2, y)
+    f_x_p, f_y_p = f_grav(m_bh, m_star, x + r_s/2, y)
 
     tidal_f_x = f_x_p - f_x_m
     tidal_f_y = f_y_p - f_y_m
 
-# setup the plot
+# rk4 method
+def rk4(x, y, v_x, v_y):
+    # calc acceleration from grav in x and y directions
+    def accel(x, y):
+        f_x, f_y = f_grav(m_bh, m_star, x, y)
+        a_x = f_x / m_star
+        a_y = f_y / m_star
+        return a_x, a_y
+
+    # k1
+    a_x1, a_y1 = accel(x, y)
+    k1_vx = a_x1 * dt
+    k1_vy = a_y1 * dt
+    k1_x = v_x * dt
+    k1_y = v_y * dt
+
+    # k2
+    a_x2, a_y2 = accel(x + 0.5 * k1_x, y + 0.5 * k1_y)
+    k2_vx = a_x2 * dt
+    k2_vy = a_y2 * dt
+    k2_x = (v_x + 0.5 * k1_vx) * dt
+    k2_y = (v_y + 0.5 * k1_vy) * dt
+
+    # k3
+    a_x3, a_y3 = accel(x + 0.5 * k2_x, y + 0.5 * k2_y)
+    k3_vx = a_x3 * dt
+    k3_vy = a_y3 * dt
+    k3_x = (v_x + 0.5 * k2_vx) * dt
+    k3_y = (v_y + 0.5 * k2_vy) * dt
+
+    # k4
+    a_x4, a_y4 = accel(x + k3_x, y + k3_y)
+    k4_vx = a_x4 * dt
+    k4_vy = a_y4 * dt
+    k4_x = (v_x + k3_vx) * dt
+    k4_y = (v_y + k3_vy) * dt
+
+    # combine k1, k2, k3, k4
+    x += (k1_x + 2*k2_x + 2*k3_x + k4_x) / 6
+    y += (k1_y + 2*k2_y + 2*k3_y + k4_y) / 6
+    v_x += (k1_vx + 2*k2_vx + 2*k3_vx + k4_vx) / 6
+    v_y += (k1_vy + 2*k2_vy + 2*k3_vy + k4_vy) / 6
+
+    return x, y, v_x, v_y
+
+# define plot
 fig, ax = plt.subplots(figsize=(6, 6))
-ax.set_xlim(-r_star*10, r_star*10)
-ax.set_ylim(-r_star*10, r_star*10)
-ax.set_aspect('equal', 'box')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
+ax.set_xlim(-r_bh*15, r_bh*15)
+ax.set_ylim(-r_bh*15, r_bh*15)
 
-# create scatter plot for particles
-scatter = ax.scatter(x, y)
-plt.show()
-    
+# plot black hole
+ax.plot(0, 0, 'o', color='black', markersize=10)
+ax.add_patch(plt.Circle((0, 0), r_bh, color='black', fill=False, linestyle='dashed'))
+
+# create scatter plot for star particles
+star1 = ax.scatter(x1, y1, s=0.2, c='orange')
+
+# initialize plot
+def init():
+    star1.set_offsets(np.empty((0, 2)))
+    return star1, 
+
+# update plot across time steps
 def update(frame):
-    #bleh
-
+    global x1, y1, v_x1, v_y1
     
+    x1, y1, v_x1, v_y1 = rk4(x1, y1, v_x1, v_y1)
 
+    # in the case star falls into event horizon
+    r = np.sqrt(x1**2 + y1**2)
+    mask = r <= r_bh
+    x1[mask], y1[mask] = 0, 0
+    v_x1[mask], v_y1[mask] = 0, 0
 
+    star1.set_offsets(np.column_stack((x1, y1)))
+
+    return star1,
+
+# plot animation
+ani = animation.FuncAnimation(fig, update, frames=500, init_func=init, blit=True, interval=10)
+plt.show()
